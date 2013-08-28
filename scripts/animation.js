2cf02f846438abdb040403df58c85939d54671f6
MrmacHD <mrmachd@gmail.com>
2013-08-29 03:10:42 +0430

    phase 1

---
+++ scripts/animation.js
@@ -0,0 +1,17 @@
+/*
+ * animate anchor links
+ */
+$(document).ready(function() {
+  $(document).on("click", "a[href*=#]", function() {
+    if (location.pathname.replace(/^\//,"") == this.pathname.replace(/^\//,"") && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target || $("[name=" + this.hash.slice(1) +"]");
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $("html,body").animate({scrollTop: targetOffset}, 300);
+        return false;
+      }
+    }
+  });
+});
+
